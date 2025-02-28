da5ab6ad5dbbf3fc113e74182f9e93a33f9672ac
adri <04adrianc@inscastellet.cat>
2025-02-28 16:41:30 +0100

    3a pujada

---
+++ app/src/main/java/com/example/quiztrivial/LoginRegister.kt
@@ -0,0 +1,25 @@
+package com.example.quiztrivial
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+        var BTMLOGIN = findViewById<Button>(R.id.BTMLOGIN);
+        var BTMREGISTRO = findViewById<Button>(R.id.BTMREGISTRO);
+        BTMLOGIN.setOnClickListener(){
+            Toast.makeText(this, "click botó login",Toast.LENGTH_LONG).show();
+        }
+        BTMREGISTRO.setOnClickListener(){
+            Toast.makeText(this, "click botó Registre",Toast.LENGTH_LONG).show();
+        }
+    }
+}
